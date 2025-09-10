a7ef624fa020a9280edf9d46e123934427e41936
AchaUmbaPretre <achandambi@gmail.com>
2025-09-10 13:35:17 +0100

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,40 @@
-import ModeTv from '../modeTv/ModeTv'
-import RapportVehiculeValide from '../rapportVehiculeValide/RapportVehiculeValide'
-import RapportVehiculeCourses from '../rapportVehiculeCourses/RapportVehiculeCourses'
-import RapportVehiculeUtilitaire from '../rapportVehiculeUtilitaire/RapportVehiculeUtilitaire'
+import { useState, useEffect } from 'react';
+import ModeTv from '../modeTv/ModeTv';
+import RapportVehiculeValide from '../rapportVehiculeValide/RapportVehiculeValide';
+import RapportVehiculeCourses from '../rapportVehiculeCourses/RapportVehiculeCourses';
+import RapportVehiculeUtilitaire from '../rapportVehiculeUtilitaire/RapportVehiculeUtilitaire';
+import './home.scss';
+
+const componentsList = [
+  <ModeTv key="modeTv" />,
+  <RapportVehiculeValide key="valide" />,
+  <RapportVehiculeCourses key="courses" />,
+  <RapportVehiculeUtilitaire key="utilitaire" />
+];
 
 const Home = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false); // commence le fade-out
+      setTimeout(() => {
+        setCurrentIndex(prev => (prev + 1) % componentsList.length);
+        setFade(true); // fade-in le nouveau composant
+      }, 500); // durée du fade-out
+    }, 3000); // toutes les 3 secondes
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>
-        <div className="home">
-            <ModeTv/>
-            <RapportVehiculeValide/>
-            <RapportVehiculeCourses/>
-            <RapportVehiculeUtilitaire/>
-        </div>
-    </>
-  )
+    <div className="home">
+      <div className={`fade-container ${fade ? 'fade-in' : 'fade-out'}`}>
+        {componentsList[currentIndex]}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
