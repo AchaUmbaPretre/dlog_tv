22637c5ac5fecff31d1ac3041879eec89fad4896
AchaUmbaPretre <achandambi@gmail.com>
2025-09-10 12:31:58 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useSelector } from 'react-redux';
 import './App.css';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+
 
 function App() {
+  const userId = useSelector((state) => state.user?.currentUser?.id_utilisateur);
+
+  const SecureRoute = ({ children }) => {
+    if (!userId) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    { path: '/', element: <Home />},
+    { path: '/login', element: <Login /> }
+  ]);
+
   return (
     <div className="App">
-      ACHAAAAAAAAAA
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -13,39 +13,14 @@ export const login = async (dispatch, user, navigate) => {
     dispatch(loginSuccess(res.data));
     if (res.data.success) {
       message.success("Connectez-vous avec succès");
-      const role = res.data?.role;
-
-      if (role === 'Securité') {
-        navigate('/securite/dashboard');
-      } else {
+      
         navigate('/');
-      }
-
     } else {
       navigate('/login')
       message.error(res.data.message);
     }
   } catch (err) {
     dispatch(loginFailure());
-  }
-};
-
-export const register = async (dispatch, user) => {
-  const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
-
-  dispatch(loginStart());
-  try {
-    const res = await axios.post(`${DOMAIN}/api/auth/register`, user);
-    dispatch(loginSuccess(res.data));
-    if (res.data.success) {
-      message.success("Les informations sont enregistrées avec succès");
-    } else {
-      message.error(res.data.message);
-    }
-  } catch (err) {
-    dispatch(loginFailure());
-    console.log(err);
-    message.error("Quelque chose s'est mal passé");
   }
 };
 
