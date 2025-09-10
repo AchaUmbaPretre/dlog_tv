beabe90168a7a8c8e7c3f771c7c236e8110bdf73
AchaUmbaPretre <achandambi@gmail.com>
2025-09-10 11:35:08 +0100

    commit

---
+++ src/config.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
+const config = {
+   REACT_APP_SERVER_DOMAIN : 'http://localhost:8080'
+
+/*   REACT_APP_SERVER_DOMAIN : 'https://apidlog.loginsmart-cd.com'
+ */ };
+
+export default config;
+
+export const userRequest = axios.create({
+  baseURL: 'https://apidlog.loginsmart-cd.com',
+  headers: { Authorization: `Bearer ${TOKEN}` },
+});
---
+++ src/services/authService.js
@@ -0,0 +1,17 @@
+// src/services/authService.js
+import axios from 'axios';
+import config from '../config';
+
+const DOMAIN = config.REACT_APP_SERVER_DOMAIN
+
+export const loginUser = async (user) => {
+  return axios.post(`${DOMAIN}/api/auth/login`, user);
+};
+
+export const registerUser = async (user) => {
+  return axios.post(`${DOMAIN}/api/auth/register`, user);
+};
+
+export const logout = async (user) => {
+    return axios.post(`${DOMAIN}/api/auth/logout`, user);
+  };
